4b1d123741a6df3f642f59026eebb33012d09cfd
diegoleteliers10 <diegoleteliers10@gmail.com>
2023-05-14 23:14:12 -0400

    ajustes de index.html

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -5,20 +5,20 @@ import styles  from './Card.module.css'
 const Card = (props) => {
 
   //traemos los datos necesarios a usar desde props
-    const { id, image, name, temperament, weight } = props
+  const { id, image, name, temperament, weight } = props
 
   //retornamos los datos de cada card
   return(
-    <>
+    <Link to={`/detail/${id}`} className={styles.cardLink}>
       <div className={styles.cardDog} >
         <img src={image} alt={name} className={styles.imgDog}/>
         <section className={styles.cardInfo}>
-          <Link to={`/detail/${id}`} className={styles.linkName}>{name}</Link>
+          <h2 className={styles.name}>{name}</h2>
           <p>{temperament}</p>
           <p>Peso: {weight} kg</p>
         </section>
       </div>
-    </>
+    </Link>
   )
 }
 

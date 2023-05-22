2e098b51d1c5aaa528d1ce287d64c68c09dcb0b8
diegoleteliers10 <diegoleteliers10@gmail.com>
2023-05-22 18:45:42 -0400

    update mejora pagina

--- api/src/Controllers/updateDog.js
+++ api/src/Controllers/updateDog.js
@@ -5,14 +5,23 @@ const { getAllDogs } = require('../helper/data');
 const updateDog = async (req, res) => {
   const { name, temperaments, weight, height, life_span } = req.body;
   const { idUpdate } = req.params;
-  console.log(idUpdate)
 
   try {
-    //SETEAMOS LOS NUEVOS DATOS
-    const dog = await Dog.update(
-      { name: name, height:height, weight:weight, life_span:life_span},
-      { where: { id: idUpdate } }
-    );
+    // Buscamos el perro por su ID
+    const dog = await Dog.findByPk(idUpdate);
+
+    if (!dog) {
+      return res.status(404).send({ message: 'Perro no encontrado' });
+    }
+
+    // Actualizamos los datos del perro
+    dog.name = name;
+    dog.weight = weight;
+    dog.height = height;
+    dog.life_span = life_span;
+    // Guardamos los cambios en la base de datos
+    await dog.save();
+
     //ELIMINAMOS LOS TEMPERAMENTOS ANTERIORES
     await dog.setTemperaments([]);
 
@@ -23,10 +32,8 @@ const updateDog = async (req, res) => {
       dog.addTemperament(dogTemp)
     })
 
-    //traemos a todos los perros
-    const newAllDogs= await getAllDogs();
+    const newAllDogs = await getAllDogs();
     res.status(200).send(newAllDogs);
-
   } catch (error) {
     res.status(500).send({ message: error.message });
   }
